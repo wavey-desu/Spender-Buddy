b9d13e83e9f846a0fc30d306b508e6289b325573
ALEJANDRO BARRERA FLORES <wavey.dev0@gmail.com>
2022-07-26 12:33:08 -0500

    OAuth route fix

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ passport.deserializeUser(function(id, done){
   passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/home"
+    callbackURL: "https://spender-buddy.herokuapp.com/auth/google/home"
   },
   function(accessToken, refreshToken, profile, cb) {
     // console.log(profile);
